32119e292de5cbe0fcaf184f970b1a041fcc08d6
dbianchii <gdbianchii@gmail.com>
2023-07-29 14:20:00 -0300

    Add button back

--- apps/stays-ai/src/app/page.tsx
+++ apps/stays-ai/src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Page() {
 
   return (
     <main className="bg-muted h-screen">
-      <div className="bg-background shadow-foreground fixed bottom-0 z-40 flex h-40 w-full flex-col shadow-md transition-transform">
+      <div className="bg-background shadow-foreground fixed bottom-0 z-40 flex h-48 w-full flex-col shadow-md transition-transform">
         <div className="flex h-32 flex-row p-4">
           <div className="space-x-1 space-y-1">
             {tags.map((tag, i) => (
@@ -38,11 +38,11 @@ export default function Page() {
             </Button>
           </div>
         </div>
-        <div className="flex flex-row space-x-3 p-4">
+        <div className="mb-6 flex flex-row space-x-3 p-4">
           <Input type="text" className="text-md"></Input>
-          {/* <Button variant={"secondary"}>
+          <Button variant={"secondary"}>
             <Plus className="h-4 w-4" />
-          </Button> */}
+          </Button>
         </div>
       </div>
     </main>
