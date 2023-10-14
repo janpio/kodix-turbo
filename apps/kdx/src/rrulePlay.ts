a207b079f3d2900415476031cad0be4a29271e55
Gabriel Bianchi <gdbianchii@gmail.com>
2023-10-14 13:46:07 -0300

    Fix lint

--- apps/kdx/src/rrulePlay.ts
+++ apps/kdx/src/rrulePlay.ts
@@ -1,5 +1,4 @@
 import moment from "moment";
-import type { ByWeekday } from "rrule";
 import { Frequency, RRule, Weekday } from "rrule";
 
 //http://jkbrzt.github.io/rrule/
@@ -10,7 +9,5 @@ const rule = new RRule({
   until: moment().add(2, "day").toDate(),
   byweekday: [new Weekday(2)],
 });
-
-type asd = ByWeekday;
 
 console.log(rule.all());
