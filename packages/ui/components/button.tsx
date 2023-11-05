7226412f3b0327851d19587aba6aefa488ab9658
Gabriel Bianchi <gdbianchii@gmail.com>
2023-11-05 14:24:50 -0300

    UI > not all components need to be useClient

--- packages/ui/components/button.tsx
+++ packages/ui/components/button.tsx
@@ -1,7 +1,11 @@
-//This file is separated from button.tsx so we can use it in server components
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
 import { cva } from "class-variance-authority";
+import type { VariantProps } from "class-variance-authority";
 
-export const buttonVariants = cva(
+import { cn } from "../lib/utils";
+
+const buttonVariants = cva(
   "inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
@@ -30,3 +34,25 @@ export const buttonVariants = cva(
     },
   },
 );
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button";
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
+);
+Button.displayName = "Button";
+
+export { Button, buttonVariants };
