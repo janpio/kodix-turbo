9a9c72a43ee83e731b65a746c35d5b89200a3110
Gabriel Bianchi <gdbianchii@gmail.com>
2023-11-05 11:44:11 -0300

    fix buttonVariants

--- packages/ui/components/alert-dialog.tsx
+++ packages/ui/components/alert-dialog.tsx
@@ -4,7 +4,7 @@ import * as React from "react";
 import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog";
 
 import { cn } from "../lib/utils";
-import { buttonVariants } from "./button/button";
+import { buttonVariants } from "./button/buttonVariants";
 
 const AlertDialog = AlertDialogPrimitive.Root;
 
