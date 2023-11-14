9b3ee775f452459e3da4a2e2bee986a5b3e96513
Gabriel Bianchi <gdbianchii@gmail.com>
2023-11-14 13:19:44 -0300

    isPlanetScaleConnection ?

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -3,18 +3,21 @@ import { PrismaPlanetScale } from "@prisma/adapter-planetscale";
 import { PrismaClient } from "@prisma/client";
 import { fetch as undiciFetch } from "undici";
 
+//* START PLANETSCALE ADAPTER SECTION
 const client = new Client({
   url: `${process.env.DATABASE_URL}`,
   fetch: undiciFetch,
 });
-
 const adapter = new PrismaPlanetScale(client);
+const isPlanetScaleConnection = `${process.env.DATABASE_URL}`.includes("psdb");
+//* END PLANETSCALE ADAPTER SECTION
+
 const globalForPrisma = globalThis as { prisma?: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    adapter: process.env.NODE_ENV === "development" ? null : adapter, //?  Only use Planetscale adapter in production
+    adapter: isPlanetScaleConnection ? adapter : null, //?  Only use Planetscale adapter if we are connecting to Planetscale
     log:
       process.env.NODE_ENV === "development"
         ? ["query", "error", "warn"]
