4a0d9063273ec4775125c12182dbe01b691c7c14
Gabriel Bianchi <gdbianchii@gmail.com>
2023-11-16 01:06:11 -0300

    BRO

--- apps/kdx/src/app/page.tsx
+++ apps/kdx/src/app/page.tsx
@@ -1,47 +1,36 @@
-import { unstable_cache } from "next/cache";
-
-import type { RouterOutputs } from "@kdx/api";
+import { getBaseUrl } from "@kdx/api/src/shared";
 import { auth } from "@kdx/auth";
+import { AvatarWrapper } from "@kdx/ui";
 
-import { KodixApp } from "~/app/_components/app/kodix-app";
-import { api } from "~/trpc/server";
 import { GradientHero } from "./_components/gradient-hero";
 
 export default async function Home() {
   const session = await auth();
-
-  let initialData: RouterOutputs["app"]["getAll"] | undefined;
-  if (session) {
-    const getApps = unstable_cache(
-      async () => await api.app.getAll.query(),
-      ["apps"],
-      {
-        tags: ["apps"],
-      },
-    )();
-
-    initialData = await getApps;
-  }
 
   return (
     <div className="h-144 flex min-h-screen flex-col items-center gap-12 px-4 py-16">
       <h1 className="text-primary scroll-m-20 bg-gradient-to-r from-purple-400 to-pink-600 bg-clip-text text-6xl font-extrabold tracking-tight text-transparent lg:text-8xl">
         Welcome to Kodix
       </h1>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-4">
-        {initialData?.map((app) => (
-          <div key={app.id}>
-            <KodixApp
-              id={app.id}
-              appName={app.name}
-              appDescription={app.description}
-              appUrl={app.urlApp}
-              installed={app.installed}
-              session={session}
+      <div></div>
+      {session && (
+        <div className="text-center">
+          <p className="font-italic italic">You are on</p>
+          <div className="flex">
+            <AvatarWrapper
+              className="mr-2 h-5 w-5"
+              src={`${getBaseUrl()}/api/avatar/${
+                session.user.activeWorkspaceName
+              }`}
+              alt={session.user.activeWorkspaceName}
+              fallback={session.user.activeWorkspaceName}
             />
+            <span className="bg-gradient-to-r from-purple-400 to-pink-600 bg-clip-text font-bold">
+              {session.user.activeWorkspaceName}
+            </span>
           </div>
-        ))}
-      </div>
+        </div>
+      )}
       <GradientHero />
     </div>
   );
