e34c9303faee88d31e6a9900a094d049d023a339
dbianchii <gdbianchii@gmail.com>
2023-11-19 22:47:30 -0300

    bro

--- packages/auth/index.ts
+++ packages/auth/index.ts
@@ -158,13 +158,6 @@ export const {
       ).activeWorkspaceId;
       return session;
     },
-    redirect({ url, baseUrl }) {
-      // Allows relative callback URLs
-      if (url.startsWith("/")) return `${baseUrl}${url}`;
-      // Allows callback URLs on the same origin
-      else if (new URL(url).origin === baseUrl) return url;
-      return baseUrl;
-    },
   },
   pages: {
     signIn: "/signin",
