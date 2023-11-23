186e944f58cd638a8a7c429fbfe20c5be113c5d7
Gabriel Bianchi <gdbianchii@gmail.com>
2023-11-23 14:55:42 -0300

    redirect to /workspace

--- apps/kdx/src/app/page.tsx
+++ apps/kdx/src/app/page.tsx
@@ -1,3 +1,5 @@
+import { redirect } from "next/navigation";
+
 import { getBaseUrl } from "@kdx/api/src/shared";
 import { auth } from "@kdx/auth";
 import { AvatarWrapper } from "@kdx/ui";
@@ -6,6 +8,7 @@ import { GradientHero } from "./_components/gradient-hero";
 
 export default async function Home() {
   const session = await auth();
+  if (session) return redirect("/workspace");
 
   return (
     <div className="h-144 flex min-h-screen flex-col items-center gap-12 px-4 py-16">
@@ -13,24 +16,6 @@ export default async function Home() {
         Welcome to Kodix
       </h1>
       <div></div>
-      {session && (
-        <div className="text-center">
-          <p className="font-italic italic">You are on</p>
-          <div className="flex">
-            <AvatarWrapper
-              className="mr-2 h-5 w-5"
-              src={`${getBaseUrl()}/api/avatar/${
-                session.user.activeWorkspaceName
-              }`}
-              alt={session.user.activeWorkspaceName}
-              fallback={session.user.activeWorkspaceName}
-            />
-            <span className="bg-gradient-to-r from-purple-400 to-pink-600 bg-clip-text font-bold">
-              {session.user.activeWorkspaceName}
-            </span>
-          </div>
-        </div>
-      )}
       <GradientHero />
     </div>
   );
