5f6caf50892d6a5398547850a3c41a988ad5ebd4
Gabriel Bianchi <gdbianchii@gmail.com>
2023-11-23 14:57:13 -0300

    fixity

--- apps/kdx/src/app/_components/providers.tsx
+++ apps/kdx/src/app/_components/providers.tsx
@@ -1,16 +1,15 @@
 "use client";
 
-import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 import type { ThemeProviderProps } from "next-themes/dist/types";
 
-export const NextAuthProvider = ({
-  children,
-}: {
-  children?: React.ReactNode;
-}) => {
-  return <SessionProvider>{children}</SessionProvider>;
-};
+//export const NextAuthProvider = ({
+//  children,
+//}: {
+//  children?: React.ReactNode;
+//}) => {
+//  return <SessionProvider>{children}</SessionProvider>;
+//}; //? This is not being used at the moment.
 
 export function NextThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <ThemeProvider {...props}>{children}</ThemeProvider>;
