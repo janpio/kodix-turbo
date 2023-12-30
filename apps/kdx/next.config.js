ec0e8a8bb58a5ddbfa145eefb2ca3de95db422d4
Gabriel Bianchi <gdbianchii@gmail.com>
2023-12-30 11:10:43 -0300

    Add tailwindCSS classRegex and transpile @kdx/validators package

--- apps/kdx/next.config.js
+++ apps/kdx/next.config.js
@@ -7,7 +7,13 @@ const config = {
   reactStrictMode: true,
 
   /** Enables hot reloading for local packages without a build step */
-  transpilePackages: ["@kdx/api", "@kdx/auth", "@kdx/db", "@kdx/ui"],
+  transpilePackages: [
+    "@kdx/api",
+    "@kdx/auth",
+    "@kdx/db",
+    "@kdx/ui",
+    "@kdx/validators",
+  ],
 
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
