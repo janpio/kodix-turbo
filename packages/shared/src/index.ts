41ac913e5c64ef4d18a5b1925367bfd2fc4fc5d5
Gabriel Bianchi <gdbianchii@gmail.com>
2023-12-30 20:28:16 -0300

    Make ui more inline with turbo

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,5 +1,4 @@
 //? Note: anything exported in this file must be something that is safe to be shared between the client and the server
 
 export * from "./utils";
-export * from "./schemas";
 export * from "./db";
