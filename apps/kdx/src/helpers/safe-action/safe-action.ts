b454ffafeabad6b9bf1c986f4871e08fe2bffb1a
Gabriel Bianchi <gdbianchii@gmail.com>
2024-01-08 14:27:32 -0300

    Fix safe action

--- apps/kdx/src/helpers/safe-action/safe-action.ts
+++ apps/kdx/src/helpers/safe-action/safe-action.ts
@@ -1,6 +1,6 @@
-import type { typeToFlattenedError } from "zod";
-import { TRPCClientError } from "@trpc/client";
+import { TRPCError } from "@trpc/server";
 import { createSafeActionClient } from "next-safe-action";
+import { ZodError } from "zod";
 
 //? This is from https://next-safe-action.dev/docs/getting-started
 export const action = createSafeActionClient({
@@ -8,29 +8,11 @@ export const action = createSafeActionClient({
   handleReturnedServerError(error) {
     let message = error.message;
 
-    //? This is a super hacky and weird solution i found to get the error message from inside
-    //? trpc's zod error. This is not the logic for the server action's zod.
-    interface TrpcErrorData {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      zodError?: typeToFlattenedError<any, string>;
-      code: string;
-      httpStatus: number;
-      path: string;
-      stack: string;
-    }
-    if (error instanceof TRPCClientError && typeof error.data === "object") {
-      const data = error.data as TrpcErrorData;
-      if (data.zodError) {
-        const firstZodContentError = Object.values(
-          data?.zodError?.fieldErrors,
-        )?.[0]?.[0];
-        const firstZodFormError = Object.values(
-          data?.zodError?.formErrors,
-        )?.[0]?.[0];
-
-        message = firstZodContentError ?? firstZodFormError ?? message;
-      }
-    }
+    //? If the error came from within tRPC and not from the outer action, we can check if it's a ZodError and use the first issue's message.
+    //? Note that if it's a generic trpc error and not a ZodError, we'll just use the original error message. (should work)
+    if (error instanceof TRPCError)
+      if (error.cause instanceof ZodError)
+        message = error.cause.issues[0]?.message ?? message;
 
     return {
       serverError: message ?? "Something went wrong, please try again later.",
