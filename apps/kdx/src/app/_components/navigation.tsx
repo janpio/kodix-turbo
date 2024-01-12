314c09aa7c63f2065abf0b426b2ca5f599e052a6
Gabriel Bianchi <gdbianchii@gmail.com>
2024-01-12 10:28:35 -0300

    just commit

--- apps/kdx/src/app/_components/navigation.tsx
+++ apps/kdx/src/app/_components/navigation.tsx
@@ -4,6 +4,12 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { ArrowLeft } from "lucide-react";
 
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@kdx/ui/navigation-menu";
 import { navigationMenuTriggerStyle } from "@kdx/ui/navigationMenuTriggerStyle";
 import { cn, useMediaQuery } from "@kdx/ui/utils";
 
@@ -21,28 +27,28 @@ export function Navigation({
   }[];
 }) {
   const pathname = usePathname();
-  const matches = useMediaQuery({ query: "md" });
+  const isSmallerScreen = useMediaQuery({ query: "md" });
   const entryPoint = goBackItem.href.split("/").at(-1);
   if (!entryPoint) throw new Error("Your goBackItem.href is invalid");
 
   return (
-    <div
-      className={cn(
-        "flex w-full flex-col items-center space-y-2 self-start md:w-min",
-      )}
-    >
-      {!pathname.endsWith(entryPoint) && !matches ? (
-        <NavigationItem href={goBackItem.href}>
-          <ArrowLeft className="mr-2 h-4 w-4" /> {goBackItem.title}
-        </NavigationItem>
-      ) : (
-        items.map((item, i) => (
-          <NavigationItem key={i} href={item.href}>
-            {item.title}
+    <NavigationMenu className="flex w-full max-w-4xl self-start">
+      <NavigationMenuList
+        className={cn("asdas flex w-full flex-col space-y-2")}
+      >
+        {!pathname.endsWith(entryPoint) && !isSmallerScreen ? (
+          <NavigationItem href={goBackItem.href}>
+            <ArrowLeft className="mr-2 h-4 w-4" /> {goBackItem.title}
           </NavigationItem>
-        ))
-      )}
-    </div>
+        ) : (
+          items.map((item, i) => (
+            <NavigationItem key={i} href={item.href}>
+              {item.title}
+            </NavigationItem>
+          ))
+        )}
+      </NavigationMenuList>
+    </NavigationMenu>
   );
 }
 
@@ -53,15 +59,21 @@ function NavigationItem({
   href: string;
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+
   return (
-    <Link
-      className={cn(
-        navigationMenuTriggerStyle(),
-        "w-full justify-start font-bold md:w-60",
-      )}
-      href={href}
-    >
-      {children}
-    </Link>
+    <NavigationMenuItem>
+      <Link href={href} legacyBehavior passHref>
+        <NavigationMenuLink
+          active={pathname === href}
+          className={cn(
+            navigationMenuTriggerStyle(),
+            "justify-start text-center font-bold md:w-60",
+          )}
+        >
+          {children}
+        </NavigationMenuLink>
+      </Link>
+    </NavigationMenuItem>
   );
 }
